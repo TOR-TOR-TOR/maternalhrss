db498e48e09ed3106f9d9789463ff94bb2185e0d
TOR-TOR-TOR <swazitech002@gmail.com>
2026-01-30 11:21:49 +0300

    mm

--- apps/anc/models.py
+++ apps/anc/models.py
@@ -172,7 +172,7 @@ class ANCVisit(models.Model):
         #   return "Overdue"
         #immediate below is correction for above
         elif self.scheduled_date and self.scheduled_date < date.today():
-
+            return "Overdue"
         elif self.scheduled_date == date.today():
             return "Due Today"
         else:
