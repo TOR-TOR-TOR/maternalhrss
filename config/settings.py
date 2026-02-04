b1570362b0cf3b5893c84b755ce4daffd7404a42
TOR-TOR-TOR <swazitech002@gmail.com>
2026-02-04 09:50:01 +0300

    mmm

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,13 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
-}
+}        
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
